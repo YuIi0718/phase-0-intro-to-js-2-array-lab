cf2de436841901e2e7833c9489576df578f411df
Weiyi Shen <vicky18058128335@outlook.com>
2023-05-31 11:24:55 -0400

    array hw

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function ArrayFunctions(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const cats1 = cats.slice();
+    cats1. push(name);
+    return cats1;
+}
+
+function prependCat(name){
+    const cats2 = cats.slice();
+    cats2. unshift(name);
+    return cats2;
+}
+
+function removeLastCat(){
+    const cats3 = cats.slice();
+    cats3.pop();
+    return cats3;
+}
+
+function removeFirstCat(){
+    const cats4 = cats.slice();
+    cats4.shift();
+    return cats4;
+}
